5ca1251e15f7de7c52ae8ad0bfe0e217fefa99c2
Ryan Kohl <ryanckohl@gmail.com>
2011-06-07 17:46:06 -0400

    removed pile function, added default prefixes, house cleaning

--- src/seabass/impl.clj
+++ src/seabass/impl.clj
@@ -1,6 +1,7 @@
 (ns seabass.impl
   (:import [com.hp.hpl.jena.rdf.model Model ModelFactory])
-  (:import [com.hp.hpl.jena.query QueryFactory QueryExecutionFactory ResultSet ResultSetFormatter])
+  (:import [com.hp.hpl.jena.query QueryFactory QueryExecutionFactory
+	    ResultSet ResultSetFormatter])
   (:import [com.hp.hpl.jena.reasoner.rulesys GenericRuleReasonerFactory Rule])
   (:import [com.hp.hpl.jena.vocabulary ReasonerVocabulary])
   (:import [com.hp.hpl.jena.util FileUtils])
@@ -23,26 +24,25 @@
 	klass (str (class x))	]
     (or (= klass m) (= klass i)) ))
 		
-(defn add-model-impl [model addition]
-  (.add model addition))
-		
 (defn save-model-impl [model target]
   (with-open [ stream 	(java.io.FileOutputStream. target)]
     (let [m (.add (ModelFactory/createDefaultModel) model)
-	  p (.getProperty model "http://jena.hpl.hp.com/2003/RuleReasoner#" "ruleMode" ) ]
+	  p (.getProperty model
+			  "http://jena.hpl.hp.com/2003/RuleReasoner#"
+			  "ruleMode" ) ]
       (.write (.removeAll m nil p nil) stream "N-TRIPLE")
       target )))
 		
 (defn get-model  
-  ( [] (ModelFactory/createDefaultModel)	)
+  ( [] (ModelFactory/createDefaultModel))
   ( [url-filename]
-      (get-model url-filename (FileUtils/guessLang url-filename)) )
+      (get-model url-filename (FileUtils/guessLang url-filename)))
   ( [url-filename lang]
       (let [model (get-model) ]
 	(try (let [url (java.net.URL. url-filename)]
 	       (.read model url-filename lang))
 	     (catch java.net.MalformedURLException e
-	       (.read model (java.io.FileInputStream. url-filename) "" lang) )))))
+	       (.read model (java.io.FileInputStream. url-filename) "" lang))))))
  			
 (defn registerBuiltins []
   (.register BuiltinRegistry/theRegistry builtin/diff-second)
@@ -66,28 +66,46 @@
 	    (rules? x) (.setRules reasoner (Rule/rulesFromURL x))
 	    (string? x) (.add core (get-model x)) ))
 	 (ModelFactory/createInfModel reasoner core)
-	 (catch Exception e (prn e)) )))
+	 (catch Exception e (prn e)))))
 
 (defn getValue [key map]
   (try 
     (let [ jsonVal (get-in map [key :value]) ]
-      (condp = (get-in map [key :datatype])
-	  "http://www.w3.org/2001/XMLSchema#boolean" (str (DatatypeConverter/parseBoolean jsonVal))
-	  "http://www.w3.org/2001/XMLSchema#date" (.getTimeInMillis (DatatypeConverter/parseDate jsonVal))
-	  "http://www.w3.org/2001/XMLSchema#dateTime" (.getTimeInMillis (DatatypeConverter/parseDateTime jsonVal))
-	  "http://www.w3.org/2001/XMLSchema#decimal" (.doubleValue (DatatypeConverter/parseDecimal jsonVal))
-	  "http://www.w3.org/2001/XMLSchema#double" (Double/parseDouble jsonVal)
-	  "http://www.w3.org/2001/XMLSchema#float" (DatatypeConverter/parseFloat jsonVal)
-	  "http://www.w3.org/2001/XMLSchema#integer" (DatatypeConverter/parseInt jsonVal)
-	  "http://www.w3.org/2001/XMLSchema#string" (DatatypeConverter/parseString jsonVal)
-	  "http://www.w3.org/2001/XMLSchema#time" (.getTimeInMillis (DatatypeConverter/parseTime jsonVal))
-	  jsonVal ))
-    (catch IllegalArgumentException e (prn e)) ))
+      (condp = (get-in map [key :datatype])  
+	"http://www.w3.org/2001/XMLSchema#boolean"
+	(str (DatatypeConverter/parseBoolean jsonVal))
+	
+	"http://www.w3.org/2001/XMLSchema#date"
+	(.getTimeInMillis (DatatypeConverter/parseDate jsonVal))
+	
+	"http://www.w3.org/2001/XMLSchema#dateTime"
+	(.getTimeInMillis (DatatypeConverter/parseDateTime jsonVal))
+	
+	"http://www.w3.org/2001/XMLSchema#decimal"
+	(.doubleValue (DatatypeConverter/parseDecimal jsonVal))
+	
+	"http://www.w3.org/2001/XMLSchema#double"
+	(Double/parseDouble jsonVal)
+	
+	"http://www.w3.org/2001/XMLSchema#float"
+	(DatatypeConverter/parseFloat jsonVal)
+	
+	"http://www.w3.org/2001/XMLSchema#integer"
+	(DatatypeConverter/parseInt jsonVal)
+	
+	"http://www.w3.org/2001/XMLSchema#string"
+	(DatatypeConverter/parseString jsonVal)
+	
+	"http://www.w3.org/2001/XMLSchema#time"
+	(.getTimeInMillis (DatatypeConverter/parseTime jsonVal))
+
+	jsonVal))
+    (catch IllegalArgumentException e (prn e))))
 
 (defn shallow
   "take the default decoding of a json'd result set
    and turn it into a more incanter-like vector"
-  [result] (zipmap (keys result) (map #(getValue % result) (keys result))) )
+  [result] (zipmap (keys result) (map #(getValue % result) (keys result))))
 	
 (defn format-result-set [result-set]
   (with-open [ stream (java.io.ByteArrayOutputStream.)]
@@ -95,28 +113,59 @@
     (let [decoded (json/read-json (.toString stream))
 	  results (:bindings (:results decoded)) 
 	  cols (vec (keys (first results)))
-	  data (vec (map shallow results)) ]
-      (conj [cols] data) )))
+	  data (vec (map shallow results))]
+      (conj [cols] data))))
 
-(defn makeDataset [results] (incanter/dataset (get results 0) (get results 1)) )
+(defn makeDataset [results] (incanter/dataset (get results 0) (get results 1)))
+
+(defn prefixes [query]
+  (let [p "
+prefix xsd:     <http://www.w3.org/2001/XMLSchema#> 
+prefix rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+prefix rdfs:     <http://www.w3.org/2000/01/rdf-schema#> 
+prefix owl:     <http://www.w3.org/2002/07/owl#>  \n"]
+    (str p query)))
 
 (defn execute-select [query target]
   (try 
     (cond (string? target)
-	  (makeDataset (format-result-set (.execSelect (QueryExecutionFactory/sparqlService target query))))
-	  (model? target)	
-	  (makeDataset (format-result-set (.execSelect (QueryExecutionFactory/create (QueryFactory/create query) target)))) )
-    (catch QueryExceptionHTTP e (prn e)) ))
+	  (-> target
+	      (QueryExecutionFactory/sparqlService ,,,  (prefixes query))
+	      .execSelect
+	      format-result-set
+	      makeDataset)
+	  (model? target)
+	  (-> (prefixes query)
+	      QueryFactory/create
+	      (QueryExecutionFactory/create ,,, target)
+	      .execSelect
+	      format-result-set
+	      makeDataset))
+    (catch QueryExceptionHTTP e (prn e))))
 
 (defn execute-ask 
   [query target]
   (try
-    (cond (string? target) (.execAsk (QueryExecutionFactory/sparqlService target query))
-	  (model? target) (.execAsk (QueryExecutionFactory/create (QueryFactory/create query) target)) )
-    (catch QueryExceptionHTTP e (prn e)) ))
+    (cond (string? target)
+	  (-> target
+	      (QueryExecutionFactory/sparqlService ,,, (prefixes query))
+	      .execAsk)
+	  (model? target)
+	  (-> (prefixes query)
+	      QueryFactory/create
+	      (QueryExecutionFactory/create ,,, target)
+	      .execAsk))
+    (catch QueryExceptionHTTP e (prn e))))
 		
 (defn execute-construct [query target]
   (try 
-    (cond (string? target) (.execConstruct (QueryExecutionFactory/sparqlService target query))
-	  (model? target) (.execConstruct (QueryExecutionFactory/create (QueryFactory/create query) target)) )
-    (catch QueryExceptionHTTP e (prn e)) ))
+    (cond (string? target)
+	  (-> target
+	      (QueryExecutionFactory/sparqlService ,,, (prefixes query))
+	      .execConstruct)
+	  (model? target)
+	  (-> (prefixes query)
+	      QueryFactory/create
+	      (QueryExecutionFactory/create ,,, target)
+	      .execConstruct))
+    (catch QueryExceptionHTTP e (prn e))))
--- test/seabass/test/core.clj
+++ test/seabass/test/core.clj
@@ -1,99 +1,107 @@
 (ns seabass.test.core
-	(:import [com.hp.hpl.jena.rdf.model Model])
-	(:use [seabass.core] :reload)
-	(:use [clojure.test])
-	(:require 	[incanter.core :as incanter]	))
-  
+  (:import [com.hp.hpl.jena.rdf.model Model])
+  (:use [seabass.core] :reload)
+  (:use [clojure.test])
+  (:require 	[incanter.core :as incanter]	))
+
+(defn sb [q](str "prefix sb: <http://seabass.foo/> " q))
+
 (deftest local-test
-	(let [ m  (build "data/test.ttl" ["data/test.nt" "N-TRIPLES"]) 
-			s1 "select distinct ?p where { ?s ?p ?o }"
-			s2 "prefix sb: <http://seabass.foo/> select ?x ?y where { ?x sb:neighbor ?y }"
-			a1 "prefix sb: <http://seabass.foo/> ask {sb:olivia sb:caught sb:carl}"
-			a2 "prefix sb: <http://seabass.foo/> ask {sb:carl sb:caught sb:olivia}"
-			c1 "prefix sb: <http://seabass.foo/> construct { ?x sb:neighbor ?y } { ?p sb:caught ?x . ?p sb:caught ?y . filter( ?x != ?y )} "
-			]
-		(is (= 18 (incanter/nrow (bounce s1 m))))
-		(is (ask a1 m))
-		(is (not (ask a2 m)))
-		(is (= 0 (incanter/nrow(bounce s2 m))))
-		(is (= 2 (incanter/nrow (bounce s2 (pull c1 m)))))
-		(is (= 19 (incanter/nrow (bounce s1 (build m (pull c1 m))))))	))
-		
-(deftest add-test
-	(let [ m 	(build "data/test.ttl" (build "data/test.nt"))
-			 n 	(pile (build "data/test.ttl") (build "data/test.nt")) ]
-			(is (.isIsomorphicWith m n))	))
-		
+  (let [m  (build "data/test.ttl" ["data/test.nt" "N-TRIPLES"]) 
+	s1 "select distinct ?p where { ?s ?p ?o }"
+	s2 (sb "select ?x ?y where { ?x sb:neighbor ?y }")
+	a1 (sb "ask {sb:olivia sb:caught sb:carl}")
+	a2 (sb "ask {sb:carl sb:caught sb:olivia}")
+	c1 (sb "construct { ?x sb:neighbor ?y }
+{ ?p sb:caught ?x . ?p sb:caught ?y . filter( ?x != ?y )}")]
+    (is (= 18 (incanter/nrow (bounce s1 m))))
+    (is (ask a1 m))
+    (is (not (ask a2 m)))
+    (is (= 0 (incanter/nrow(bounce s2 m))))
+    (is (= 2 (incanter/nrow (bounce s2 (pull c1 m)))))
+    (is (= 19 (incanter/nrow (bounce s1 (build m (pull c1 m))))))))
+	       	
 (deftest remote-test
-	(let [	s1 "select ?x where { ?x a <http://seabass.foo/Fish>  } limit 10"
-			s2 "select ?p where { ?s ?p ?o }"
-			c1 "construct {?x a <http://seabass.foo/Fish>} where { ?x a <http://www4.wiwiss.fu-berlin.de/factbook/ns#Country>}"
-			endpoint "http://www4.wiwiss.fu-berlin.de/factbook/sparql"
-			remote-xml "http://id.southampton.ac.uk/dataset/apps/latest.rdf"
-			remote-ttl "http://id.southampton.ac.uk/dataset/apps/latest.ttl"
-			m (build "data/test.ttl" "data/test.nt")	]
-		(is (= 3 (incanter/nrow (bounce s1 m))))
-		(is (= 10 (incanter/nrow (bounce s1 (build m (pull c1 endpoint))))))
-		(is (< 0 (incanter/nrow (bounce s2 (build [remote-xml "RDF/XML"])))))
-		(is (< 0 (incanter/nrow (bounce s2 (build [remote-ttl "TTL"])))))	))
-		
+  (let [s1 "select ?x where { ?x a <http://seabass.foo/Fish>  } limit 10"
+	s2 "select ?p where { ?s ?p ?o }"
+	c1 "construct {?x a <http://seabass.foo/Fish>} where { ?x a <http://www4.wiwiss.fu-berlin.de/factbook/ns#Country>}"
+	endpoint "http://www4.wiwiss.fu-berlin.de/factbook/sparql"
+	remote-xml "http://id.southampton.ac.uk/dataset/apps/latest.rdf"
+	remote-ttl "http://id.southampton.ac.uk/dataset/apps/latest.ttl"
+	m (build "data/test.ttl" "data/test.nt")]
+    (is (= 3 (incanter/nrow (bounce s1 m))))
+    (is (= 10 (incanter/nrow (bounce s1 (build m (pull c1 endpoint))))))
+    (is (< 0 (incanter/nrow (bounce s2 (build [remote-xml "RDF/XML"])))))
+    (is (< 0 (incanter/nrow (bounce s2 (build [remote-ttl "TTL"])))))))
+
 (deftest reasoning-test
-	(let [ m  (build "data/test.ttl" "data/test.nt") 
-			s1 "prefix sb: <http://seabass.foo/> select ?x ?y where { ?x sb:neighbor ?y }"
-			r	"data/test.rules" ]
-		(is (= 0 (incanter/nrow (bounce s1 m))))
-		(is (= 2 (incanter/nrow (bounce s1 (build m r)))))	))
-			
+  (let [ m  (build "data/test.ttl" "data/test.nt") 
+	s1 (sb "select ?x ?y where { ?x sb:neighbor ?y }")
+	r "data/test.rules" ]
+    (is (= 0 (incanter/nrow (bounce s1 m))))
+    (is (= 2 (incanter/nrow (bounce s1 (build m r)))))))
+
 (deftest datatype-test
-	(let [ m (build "data/test.nt")
-			s1 "prefix sb: <http://seabass.foo/> select ?y where { ?x sb:booleans ?y } order by ?y" 
-			s2 "prefix sb: <http://seabass.foo/> select ?y where { ?x sb:dates ?y } order by ?y" 
-			s3 "prefix sb: <http://seabass.foo/> select ?y where { ?x sb:datetimes ?y } order by ?y" 
-			s4 "prefix sb: <http://seabass.foo/> select ?y where { ?x sb:decimals ?y } order by ?y" 
-			s5 "prefix sb: <http://seabass.foo/> select ?y where { ?x sb:doubles ?y } order by ?y" 
-			s6 "prefix sb: <http://seabass.foo/> select ?y where { ?x sb:floats ?y } order by ?y" 
-			s7 "prefix sb: <http://seabass.foo/> select ?y where { ?x sb:integers ?y } order by ?y" 
-			s8 "prefix sb: <http://seabass.foo/> select ?y where { ?x sb:strings ?y } order by ?y" 
-			s9 "prefix sb: <http://seabass.foo/> select ?y where { ?x sb:times ?y } order by ?y" ]
-		(is (= "false" 				(incanter/sel (bounce s1 m) :cols :y :rows 0)))	
-		(is (= "true" 				(incanter/sel (bounce s1 m) :cols :y :rows 1)))
-		(is (= -16098156000000 	(incanter/sel (bounce s2 m) :cols :y :rows 0)))	
-		(is (= 239605200000 		(incanter/sel (bounce s2 m) :cols :y :rows 1)))
-		(is (= -16098140964000 		(incanter/sel (bounce s3 m) :cols :y :rows 0)))	
-		(is (= 239653200000 			(incanter/sel (bounce s3 m) :cols :y :rows 1)))
-		(is (= -22.222 				(incanter/sel (bounce s4 m) :cols :y :rows 0)))	
-		(is (= 22.222			 		(incanter/sel (bounce s4 m) :cols :y :rows 1)))
-		(is (= -99.999 			(incanter/sel (bounce s5 m) :cols :y :rows 0)))	
-		(is (= 99.999				(incanter/sel (bounce s5 m) :cols :y :rows 1)))
-		(is (= -11 						(.intValue (incanter/sel (bounce s6 m) :cols :y :rows 0))))
-		(is (= 11			 			(.intValue (incanter/sel (bounce s6 m) :cols :y :rows 1))))
-		(is (= -12 					(incanter/sel (bounce s7 m) :cols :y :rows 0)))	
-		(is (= 12 					(incanter/sel (bounce s7 m) :cols :y :rows 1)))
-		(is (= "test"				 	(incanter/sel (bounce s8 m) :cols :y :rows 0)))	
-		(is (= "test"			 		(incanter/sel (bounce s8 m) :cols :y :rows 1)))
-		(is (= -6564000			(incanter/sel (bounce s9 m) :cols :y :rows 0)))	
-		(is (= 66000000 		(incanter/sel (bounce s9 m) :cols :y :rows 1))) ))
-		
+  (let [ m (build "data/test.nt")
+	s1 (sb "select ?y where { ?x sb:booleans ?y } order by ?y") 
+	s2 (sb "select ?y where { ?x sb:dates ?y } order by ?y") 
+	s3 (sb "select ?y where { ?x sb:datetimes ?y } order by ?y")
+	s4 (sb "select ?y where { ?x sb:decimals ?y } order by ?y")
+	s5 (sb "select ?y where { ?x sb:doubles ?y } order by ?y")
+	s6 (sb "select ?y where { ?x sb:floats ?y } order by ?y")
+	s7 (sb "select ?y where { ?x sb:integers ?y } order by ?y")
+	s8 (sb "select ?y where { ?x sb:strings ?y } order by ?y")
+	s9 (sb "select ?y where { ?x sb:times ?y } order by ?y") ]
+    (is (= "false" (incanter/sel (bounce s1 m) :cols :y :rows 0)))	
+    (is (= "true" (incanter/sel (bounce s1 m) :cols :y :rows 1)))
+    (is (= -16098156000000 (incanter/sel (bounce s2 m) :cols :y :rows 0)))	
+    (is (= 239605200000 (incanter/sel (bounce s2 m) :cols :y :rows 1)))
+    (is (= -16098140964000 (incanter/sel (bounce s3 m) :cols :y :rows 0)))	
+    (is (= 239653200000 (incanter/sel (bounce s3 m) :cols :y :rows 1)))
+    (is (= -22.222 (incanter/sel (bounce s4 m) :cols :y :rows 0)))	
+    (is (= 22.222 (incanter/sel (bounce s4 m) :cols :y :rows 1)))
+    (is (= -99.999 (incanter/sel (bounce s5 m) :cols :y :rows 0)))	
+    (is (= 99.999 (incanter/sel (bounce s5 m) :cols :y :rows 1)))
+    (is (= -11 (.intValue (incanter/sel (bounce s6 m) :cols :y :rows 0))))
+    (is (= 11 (.intValue (incanter/sel (bounce s6 m) :cols :y :rows 1))))
+    (is (= -12 (incanter/sel (bounce s7 m) :cols :y :rows 0)))	
+    (is (= 12 (incanter/sel (bounce s7 m) :cols :y :rows 1)))
+    (is (= "test" (incanter/sel (bounce s8 m) :cols :y :rows 0)))	
+    (is (= "test" (incanter/sel (bounce s8 m) :cols :y :rows 1)))
+    (is (= -6564000 (incanter/sel (bounce s9 m) :cols :y :rows 0)))	
+    (is (= 66000000 (incanter/sel (bounce s9 m) :cols :y :rows 1))) ))
+
 (deftest builtin-test
-	(let [ m (build "data/test.nt" "data/test.rules")
-		s1 "prefix sb: <http://seabass.foo/>  select ?x ?y where { ?x sb:closeTo-date ?y }"
-		s2 "prefix sb: <http://seabass.foo/>  select ?x ?y where { ?x sb:closeTo-datetime ?y }"
-		s3 "prefix sb: <http://seabass.foo/>  select ?x ?y where { ?x sb:closeTo-time ?y }" 
-		s4 "prefix sb: <http://seabass.foo/>  select ?x ?y where { ?x sb:closeTo-date-fail ?y }"
-		s5 "prefix sb: <http://seabass.foo/>  select ?x ?y where { ?x sb:closeTo-datetime-fail ?y }"
-		s6 "prefix sb: <http://seabass.foo/>  select ?x ?y where { ?x sb:closeTo-time-fail ?y }"
-		s7 "prefix sb: <http://seabass.foo/>  select ?x ?y where { ?x sb:closeTo-time-diff ?y }" ]
-		(is (= 2 (incanter/nrow (bounce s1 m))))
-		(is (= 2 (incanter/nrow (bounce s2 m))))
-		(is (= 2 (incanter/nrow (bounce s3 m))))	
-		(is (= 0 (incanter/nrow (bounce s4 m))))
-		(is (= 0 (incanter/nrow (bounce s5 m))))
-		(is (= 0 (incanter/nrow (bounce s6 m))))
-		(is (= 2 (incanter/nrow (bounce s7 m))))	))
-		
+  (let [ m (build "data/test.nt" "data/test.rules")
+	s1 (sb "select ?x ?y where { ?x sb:closeTo-date ?y }")
+	s2 (sb "select ?x ?y where { ?x sb:closeTo-datetime ?y }")
+	s3 (sb "select ?x ?y where { ?x sb:closeTo-time ?y }")
+	s4 (sb "select ?x ?y where { ?x sb:closeTo-date-fail ?y }")
+	s5 (sb "select ?x ?y where { ?x sb:closeTo-datetime-fail ?y }")
+	s6 (sb "select ?x ?y where { ?x sb:closeTo-time-fail ?y }")
+	s7 (sb "select ?x ?y where { ?x sb:closeTo-time-diff ?y }") ]
+    (is (= 2 (incanter/nrow (bounce s1 m))))
+    (is (= 2 (incanter/nrow (bounce s2 m))))
+    (is (= 2 (incanter/nrow (bounce s3 m))))	
+    (is (= 0 (incanter/nrow (bounce s4 m))))
+    (is (= 0 (incanter/nrow (bounce s5 m))))
+    (is (= 0 (incanter/nrow (bounce s6 m))))
+    (is (= 2 (incanter/nrow (bounce s7 m))))	))
+
 (deftest stash-test
-	(let [	m	(build "data/test.nt")
-			n  (build (stash m "data/stash-test.nt"))	
-			o	(build (stash n "data/stash-test-2.nt")) ]
-		(is (.isIsomorphicWith m n))	
-		(is (.isIsomorphicWith n o)) ))
+  (let [m (build "data/test.nt")
+	n (build (stash m "data/stash-test.nt"))	
+	o (build (stash n "data/stash-test-2.nt"))]
+    (is (.isIsomorphicWith m n))	
+    (is (.isIsomorphicWith n o)) ))
+
+(deftest prefix-test
+  (let [m (build "data/test.ttl")
+	q1 "select ?x { ?x rdf:type rdf:Property }"
+	q2 "select ?x { ?x rdfs:domain ?y }"
+	q3 "select ?x { ?x rdfs:range xsd:integer }"
+	q4 "select ?x { ?x rdf:type owl:Class }"]
+    (is (= 2 (incanter/nrow (bounce q1 m))))
+    (is (= 2 (incanter/nrow (bounce q2 m))))
+    (is (= 1 (incanter/nrow (bounce q3 m))))
+    (is (= 0 (incanter/nrow (bounce q4 m))))))
