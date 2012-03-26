a6c8e5f8e6c729882e6d83717605f84984f3bc14
Ryan Kohl <ryanckohl@gmail.com>
2012-03-25 21:39:36 -0400

    updated version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject seabass "0.3.3"
+(defproject seabass "0.4"
   :description "A library for working with RDF in the Incanter environment.  Documentation at https://github.com/ryankohl/seabass"
   :dev-dependencies [[lein-clojars "0.6.0"]]
   :dependencies [
