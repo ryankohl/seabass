d790b7f873a5851fc0da264405b12b95c7d9cf8b
Ryan Kohl <ryanckohl@gmail.com>
2012-12-12 14:49:18 -0500

    minor edits to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject seabass "2.0"
-  :description "A library for working with RDF.  Documentation at https://github.com/ryankohl/seabass"
+  :description "A library for working with RDF."
   :plugins [[lein-swank "1.4.4"]]
+  :url "https://github.com/ryankohl/seabass"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
      [org.clojure/clojure "1.4.0"]
      [org.clojure/math.numeric-tower "0.0.2"]
