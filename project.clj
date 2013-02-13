da713e933a3a92f5970ff813bcad4a991e4d9716
Ryan Kohl <ryanckohl@gmail.com>
2013-02-13 18:59:39 -0500

    fixed project syntax

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject seabass "2.0"
   :description "A library for working with RDF."
-  :plugins [[lein-swank "1.4.4"]]
   :url "https://github.com/ryankohl/seabass"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -9,5 +8,6 @@
      [org.clojure/math.numeric-tower "0.0.2"]
      [org.apache.jena/jena-arq "2.9.2"]
      ]
-  :plugins [[lein-clojars "0.9.1"]]
+  :plugins [[lein-clojars "0.9.1"]
+            [lein-swank "1.4.4"]]
   )
