8f3ae30d6cb745a81f4da155dd9ed800d2141952
Anatoly Zelenin <anatoly@zelenin.de>
2016-07-21 17:31:42 +0200

    Inlined *-impl functions

--- src/seabass/impl.clj
+++ src/seabass/impl.clj
@@ -1,20 +1,8 @@
 (ns seabass.impl
-  (:import [org.apache.jena.rdf.model Model ModelFactory])
-  (:import [org.apache.jena.query QueryFactory QueryExecutionFactory
-	    ResultSet ResultSetFormatter])
-  (:import [org.apache.jena.reasoner.rulesys GenericRuleReasonerFactory Rule])
-  (:import [org.apache.jena.vocabulary ReasonerVocabulary])
+  (:import [org.apache.jena.rdf.model ModelFactory])
   (:import [org.apache.jena.util FileUtils])
-  (:import [org.apache.jena.datatypes TypeMapper])
-  (:import [org.apache.jena.datatypes.xsd XSDDateTime])
-  (:import [org.apache.jena.reasoner.rulesys.builtins BaseBuiltin])
-  (:import [org.apache.jena.reasoner.rulesys BuiltinRegistry Util])
+  (:import [org.apache.jena.reasoner.rulesys BuiltinRegistry])
   (:import [org.apache.jena.graph NodeFactory Triple BlankNodeId])
-  (:import [org.apache.jena.sparql.modify.request QuadDataAcc UpdateDataInsert])
-  (:import [org.apache.jena.update UpdateAction UpdateExecutionFactory])
-  (:import [org.apache.jena.atlas.web.auth PreemptiveBasicAuthenticator ScopedAuthenticator])
-  (:import [java.net URI])
-  (:use [clojure.java.io])
   (:require [seabass.builtin :as builtin]
             [clojure.string :as str]))
 
@@ -27,18 +15,6 @@
         i "class org.apache.jena.rdf.model.impl.InfModelImpl"
 	klass (str (class x))	]
     (or (= klass m) (= klass i)) ))
-
-(defn stash-impl [model target]
-  (with-open [ stream (java.io.FileOutputStream. target)]
-    (let [p (.getProperty model
-			  "http://jena.hpl.hp.com/2003/RuleReasoner#"
-                          "ruleMode")]
-      (.removeAll model nil p nil)
-      (.write model stream "N-TRIPLE")
-      (.addProperty (.createResource model)
-                    ReasonerVocabulary/PROPruleMode
-                    "hybrid")
-      target )))
 
 (defn get-model
   ( [] (ModelFactory/createDefaultModel))
@@ -62,30 +38,11 @@
   (.register BuiltinRegistry/theRegistry builtin/diff-hour)
   (.register BuiltinRegistry/theRegistry builtin/diff-day) )
 
-(defn build-impl
-  ([] (get-model))
-  ([urls]
-   (let [core (ModelFactory/createDefaultModel)
-         config (.addProperty (.createResource core)
-                              ReasonerVocabulary/PROPruleMode
-                              "hybrid")
-         reasoner (.create (GenericRuleReasonerFactory/theInstance) config) ]
-     (registerBuiltins)
-     (doseq [x urls]
-       (cond
-         (file? x) (add-file core x)
-         (vector? x) (.add core (get-model (nth x 0) (nth x 1)))
-         (model? x) (.add core x)
-         (rules? x) (.setRules reasoner (Rule/rulesFromURL x))
-         (string? x) (.add core (get-model x)) ))
-     (ModelFactory/createInfModel reasoner core))))
-
-
 (defn get-value [node]
   (cond
-   (nil? node) nil
-   (.isLiteral node) (.getValue node)
-   (.isResource node) (.toString node)))
+    (nil? node) nil
+    (.isLiteral node) (.getValue node)
+    (.isResource node) (.toString node)))
 
 (defn get-solution [cols result]
   (zipmap (map keyword cols)
@@ -112,54 +69,6 @@ prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
 prefix owl:     <http://www.w3.org/2002/07/owl#>  \n"]
     (str p query)))
 
-(defn bounce-impl
-  ([query target]
-   (cond (string? target)
-         (-> target
-             (QueryExecutionFactory/sparqlService ,,,  (prefixes query))
-             .execSelect
-             format-result-set)
-         (model? target)
-         (-> (prefixes query)
-             QueryFactory/create
-             (QueryExecutionFactory/create ,,, target)
-             .execSelect
-             format-result-set)))
-  ([query target username password]
-   (cond (string? target)
-         (let [auth (PreemptiveBasicAuthenticator.
-                     (ScopedAuthenticator. (URI. target)
-                                           username
-                                           (char-array password)))]
-           (-> target
-               (QueryExecutionFactory/sparqlService ,,,  (prefixes query) auth)
-               .execSelect
-               format-result-set))
-         (model? target)
-         "Basic auth only defined for remote models")))
-
-(defn ask-impl [query target]
-    (cond (string? target)
-	  (-> target
-	      (QueryExecutionFactory/sparqlService ,,, (prefixes query))
-	      .execAsk)
-	  (model? target)
-	  (-> (prefixes query)
-	      QueryFactory/create
-	      (QueryExecutionFactory/create ,,, target)
-	      .execAsk)))
-
-(defn pull-impl [query target]
-    (cond (string? target)
-	  (-> target
-	      (QueryExecutionFactory/sparqlService ,,, (prefixes query))
-	      .execConstruct)
-	  (model? target)
-	  (-> (prefixes query)
-	      QueryFactory/create
-	      (QueryExecutionFactory/create ,,, target)
-	      .execConstruct)))
-
 (defn make-triple [s p o]
   (cond (.startsWith s "_:") (cond (uri? p) (Triple/create
                                              (NodeFactory/createBlankNode (BlankNodeId. s))
@@ -179,28 +88,3 @@ prefix owl:     <http://www.w3.org/2002/07/owl#>  \n"]
   (NodeFactory/createLiteral
    (str x)
    (.getTypeByValue type-mapper x)))
-
-(defn resource-fact-impl [s p o]
-  (cond (uri? o) (make-triple s p (NodeFactory/createURI o))
-        (.startsWith o "_:") (make-triple s p (NodeFactory/createBlankNode (BlankNodeId. o)))
-        :else (throw (Exception. "Object must be a valid uri"))))
-
-
-(defn literal-fact-impl [s p o]
-  (let [tm (TypeMapper/getInstance)]
-    (cond
-      (date? o) (let [cal (java.util.Calendar/getInstance)]
-                  (.setTime cal o)
-                  (make-triple s p (make-literal (XSDDateTime. cal) tm)))
-      :else (make-triple s p (make-literal o tm)))))
-
-(defn push-impl [m triples]
-  (let [qda (QuadDataAcc.)]
-    (doseq [t triples] (.addTriple qda t))
-    (cond (model? m) (UpdateAction/execute
-                      (UpdateDataInsert. qda)
-                      m)
-          (uri? m) (-> (UpdateExecutionFactory/createRemoteForm (UpdateDataInsert. qda) m)
-                       .execute)
-          :else (throw (Exception. (str "Target must be either a Jena model "
-                                        "or a uri to a sparql endpoint"))))))
